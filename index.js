7811aff0d074dbee9ca0040af455504772f80c58
Lewis <louislaizer4@gmail.com>
2022-06-11 13:36:49 +0300

    :hammer: Stable

---
+++ index.js
@@ -0,0 +1,18 @@
+// const { WebSocketServer } = require('ws');
+
+// const server = new WebSocketServer({ port: 4444 })
+const { createServer } = require("http")
+const { Server } = require('socket.io')
+
+const httpServer = createServer()
+const io = new Server(httpServer, {})
+
+io.on("connection", (socket) => {
+    socket.on("chat", (data) => {
+        console.log(data)
+        socket.broadcast.emit("chat",data)
+    })
+})
+
+console.log("[+] Sockets on")
+httpServer.listen(4444)
